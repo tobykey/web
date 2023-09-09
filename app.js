1e815985ba4e5daebf1744a8c8cbe7e04521ddfa
Toby <123823724+tobykey@users.noreply.github.com>
2023-09-09 18:47:36 +0900

    2nd week!
    book search

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var postsRouter = require('./routes/posts');
+var booksRouter = require('./routes/books');
+
 
 var app = express();
 
@@ -22,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
-app.use('/books', require('./routes/books'));
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* 도서관리 page. */
+/* 도서검색 page. */
 router.get('/', function(req, res, next) {
-  res.render('books', { title: '도서관리', name:'김토비' });
+  res.render('index', { title: '도서검색' , pageName:'books/search.ejs'});
 });
 
 module.exports = router;
