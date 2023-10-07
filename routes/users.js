6118038abc46770c4ca932eb66acc5e857bda86d
Toby <123823724+tobykey@users.noreply.github.com>
2023-10-07 18:37:09 +0900

    1007 업데이트

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+//로그인 페이지로 이동하는 라우터
+router.get('/login', function(req,res){
+  res.render('index',{title:'로그인', pageName:'users/login.ejs'});
+});
+
+//회원가입 페이지로 이동
+router.get('/join', function(req,res){
+  res.render('index',{title:'회원가입', pageName:'users/join.ejs'});
+});
+
 module.exports = router;
