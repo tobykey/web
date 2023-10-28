a0dda4d7732e11a6d121091f999901ac18bb2281
Toby <123823724+tobykey@users.noreply.github.com>
2023-10-28 17:49:23 +0900

    Add files via upload

--- routes/locals.js
+++ routes/locals.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
 
+//즐겨찾기 페이지 이동
+router.get('/favorite', function(req,res){
+  res.render('index', { title: '즐겨찾기', pageName:'locals/favorite.ejs'});
+});
 module.exports = router;
