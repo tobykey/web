c5747748eec4ef22ffdfbbcf97eb7f00a1309b08
Toby <123823724+tobykey@users.noreply.github.com>
2023-11-18 18:24:41 +0900

    Add files via upload

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* 게시글 페이지 */
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res,next) {
   res.render('index', { title: '게시판', pageName :'posts/list.ejs' });
 });
 
-router.get('/insert', function(req, res) {
+router.get('/insert', function(req, res,next) {
   res.render('index', { title: '글쓰기', pageName :'posts/insert.ejs' });
+});
+
+router.get('/read', function(req, res, next) {
+  const id=req.query.id;
+  res.render('index', { title: '게시글 정보', pageName :'posts/read.ejs',id});
 });
   
 module.exports = router;
